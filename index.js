dbaf11d65f14d5ab51abac9312dc3f96b34653ff
rabby-shek <rabbyshek1998@gmail.com>
2025-02-13 17:50:19 +0600

    initial push

---
+++ index.js
@@ -0,0 +1,23 @@
+// variable declaration
+// functions
+// object
+// conditional statement
+// loops
+// array
+// string operations
+// Math operations
+// class
+// async/await
+// error handling
+// regex
+// Dom
+// Bom
+// events
+
+// array of objects
+
+
+// quiz game
+// question -> 4 options
+
+
